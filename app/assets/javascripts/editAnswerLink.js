610d00acaa04f7318ed3503441a14be0c04f0e04
Valentyn Gevrasov <ValAntG@gmail.com>
2019-11-18 19:02:11 +0200

    Lesson#7 end lesson

---
+++ app/assets/javascripts/editAnswerLink.js
@@ -0,0 +1,8 @@
+$(document).on("turbolinks:load", function(){
+  $('.edit-answer-link').on('click ajax:success', function(e) {
+    e.preventDefault();
+    $(this).hide();
+    answer_id = $(this).data('answerId');
+    $('form#edit-answer-' + answer_id).show();
+  });
+});
