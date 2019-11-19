0bbf3a09006a70a6bf527c6d08371ec8fc8757c0
Valentyn Gevrasov <ValAntG@gmail.com>
2019-11-19 17:54:05 +0200

    Lesson#7 fix spec by try to edit other users question

--- spec/acceptance/edit_answer_spec.rb
+++ spec/acceptance/edit_answer_spec.rb
@@ -5,9 +5,9 @@ feature 'Answer edition', '
   As an author of answer
   I did like ot be able to edit my answer
 ' do
-  given(:user) { create(:user) }
-  given(:user2) { create(:user) }
-  given!(:question) { create(:question) }
+  given!(:user) { create(:user) }
+  given!(:user2) { create(:user) }
+  given!(:question) { create(:question, user: user) }
   given!(:answer) { create(:answer, question: question) }
 
   scenario 'Unauthenticated user try to edit question' do
@@ -39,9 +39,16 @@ feature 'Answer edition', '
         expect(page).to_not have_selector('textarea', visible: true)
       end
     end
+  end
+
+  describe 'Authentificated other user' do
+    before do
+      sign_in(user2)
+      visit question_path(question)
+    end
 
     scenario "try to edit other user's question" do
-      expect(page).to have_link 'Edit answer'
+      expect(page).to_not have_link 'Edit answer'
     end
   end
 end
