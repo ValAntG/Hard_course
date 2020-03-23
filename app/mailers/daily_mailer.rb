f0a72bb7b211cd07fe1ccfedecf2fc47a30559fb
ValAntG <ValAntG@gmail.com>
2020-03-24 00:44:39 +0200

    Lesson#17 send mail all users every day

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -1,13 +1,15 @@
 class DailyMailer < ApplicationMailer
-  # default from: 'NIXSolutions.school@mail.com'
+  default from: 'NIXSolutions.school@mail.com'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.daily_mailer.digest.subject
   #
   def digest(user)
-    @greeting = 'Hi'
+    now = Time.zone.now
+    @questions = Question.where(created_at: now - 1.day..now)
+    @user = user
 
-    mail to: user.email
+    mail to: user.email, subject: 'Daily digest'
   end
 end
